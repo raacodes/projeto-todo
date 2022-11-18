9d36c286eb143c3bcf2857eb95faea96be968d38
raacodes <poleguin@gmail.com>
2022-11-17 22:25:27 -0300

    add DataConfig

---
+++ src/main/java/br/com/magnasistemas/projetotodo/configs/DataConfig.java
@@ -0,0 +1,31 @@
+package br.com.magnasistemas.projetotodo.configs;
+
+import java.text.DateFormat;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
+@Configuration
+public class DataConfig {
+
+	public static final String DATETIME_FORMAT = "dd-MM-yyyy - HH:mm";
+	public static LocalDateTimeSerializer LOCAL_DATETIME_SERIALIZER = new LocalDateTimeSerializer(
+			DateTimeFormatter.ofPattern(DATETIME_FORMAT));
+
+	@Bean
+	@Primary
+	public ObjectMapper objectMapper() {
+		JavaTimeModule module = new JavaTimeModule();
+		module.addSerializer(LOCAL_DATETIME_SERIALIZER);
+		return new ObjectMapper().registerModule(module);
+	}
+
+}
