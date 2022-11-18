591c1b433775932d2021dd5d1c76bec8f3655c9c
raacodes <poleguin@gmail.com>
2022-11-17 23:01:01 -0300

    identando todoEntity

--- src/main/java/br/com/magnasistemas/projetotodo/entities/TodoEntity.java
+++ src/main/java/br/com/magnasistemas/projetotodo/entities/TodoEntity.java
@@ -16,10 +16,9 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import br.com.magnasistemas.projetotodo.enums.Status;
 
-
 @Entity
 @Table(name = "tb_todo")
-public class TodoEntity{
+public class TodoEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,49 +30,54 @@ public class TodoEntity{
 	@ManyToOne(fetch = FetchType.LAZY)
 	private UsuarioEntity usuarioEntity;
 	private LocalDateTime dataCriacao;
-	
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getTitulo() {
 		return titulo;
 	}
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
 	public String getDescricao() {
 		return descricao;
 	}
+
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+
 	public Status getStatus() {
 		return status;
 	}
+
 	public void setStatus(Status status) {
 		this.status = status;
 	}
+
 	@JsonIgnore
 	public UsuarioEntity getUsuarioEntity() {
 		return usuarioEntity;
 	}
+
 	public void setUsuarioEntity(UsuarioEntity usuarioEntity) {
 		this.usuarioEntity = usuarioEntity;
 	}
+
 	public LocalDateTime getDataCriacao() {
 		return dataCriacao;
 	}
+
 	public void setDataCriacao(LocalDateTime dataCriacao) {
 		this.dataCriacao = dataCriacao;
 	}
-	
-	
 
-
-
-	
 }
